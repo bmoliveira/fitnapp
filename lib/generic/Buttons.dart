0d92be3761690d8700cf5c1b989a8315c859f154
Bruno <bm.oliveira.dev@gmail.com>
2019-02-27 19:22:24 +0000

    Added progress button

--- lib/generic/Buttons.dart
+++ lib/generic/Buttons.dart
@@ -15,27 +15,40 @@ class FitTextButton extends StatelessWidget {
 
   Color _color;
 
+  bool _isUnderscored;
+  bool get isUnderscored {
+    return _isUnderscored;
+  }
+
   final VoidCallback onPressed;
 
-  FitTextButton(this._title, {titleColor, color, this.onPressed}) : super() {
-    this._titleColor = titleColor ?? ApplicationTheme.current.secondaryTextColor;
+  FitTextButton(this._title, {titleColor, color, isUnderscored, this.onPressed})
+      : super() {
+    this._isUnderscored = isUnderscored ?? true;
+    this._titleColor =
+        titleColor ?? ApplicationTheme.current.secondaryTextColor;
     this._color = color;
   }
 
   @override
   Widget build(BuildContext context) {
-    return FlatButton(
-      color: color,
-      onPressed: onPressed,
-      shape:
-          RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30)),
-      child: Center(
-          child: Container(
-              child: Text(
-        _title,
-        style:
-            TextStyle(color: titleColor, decoration: TextDecoration.underline),
-      ))),
+    return SizedBox.fromSize(
+      size: Size.fromHeight(40),
+      child: FlatButton(
+        color: color,
+        onPressed: onPressed,
+        shape:
+            RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30)),
+        child: Center(
+            child: Container(
+                child: Text(
+          _title,
+          style: TextStyle(
+              fontSize: 12,
+              color: titleColor,
+              decoration: _isUnderscored ? TextDecoration.underline : null),
+        ))),
+      ),
     );
   }
 }
@@ -54,19 +67,71 @@ class FitColorButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Container(
-        child: Center(
-          child: Text(
-            _title,
-            style: TextStyle(color: titleColor),
+    return SizedBox.fromSize(
+      size: Size.fromHeight(40),
+      child: RaisedButton(
+        child: Container(
+          child: Center(
+            child: Text(
+              _title,
+              style: TextStyle(fontSize: 18, color: titleColor),
+            ),
           ),
         ),
+        shape:
+            RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30)),
+        onPressed: onPressed,
+        color: color,
       ),
-      shape:
-          RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30)),
-      onPressed: onPressed,
-      color: color,
+    );
+  }
+}
+
+class ProgressButton extends FitColorButton {
+  final bool _isLoading;
+
+  ProgressButton(title, this._isLoading,
+      {titleColor, color, @required onPressed})
+      : super(title,
+            titleColor: titleColor, color: color, onPressed: onPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.fromSize(
+      size: Size.fromHeight(40),
+      child: RaisedButton(
+        child: Stack(children: <Widget>[
+          _isLoading
+              ? Container(
+                  alignment: Alignment(1, 0.5),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.white),
+                            strokeWidth: 2,
+                          ),
+                        ),
+                      ]),
+                )
+              : SizedBox(),
+          Center(
+            child: Text(
+              _title,
+              style: TextStyle(fontSize: 18, color: titleColor),
+            ),
+          ),
+        ]),
+        shape:
+            RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30)),
+        disabledColor: _isLoading ? color : null,
+        onPressed: _isLoading ? null : onPressed,
+        color: color,
+      ),
     );
   }
 }
