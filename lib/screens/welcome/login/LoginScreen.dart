61475eae2cf26e01111c1deef4f49c50893a5967
Bruno <bm.oliveira.dev@gmail.com>
2019-03-01 09:41:18 +0000

    Refactored screen to have better alignment

--- lib/screens/welcome/login/LoginScreen.dart
+++ lib/screens/welcome/login/LoginScreen.dart
@@ -1,4 +1,3 @@
-import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
@@ -6,11 +5,10 @@ import 'package:gym_track/configuration/AppIcons.dart';
 import 'package:gym_track/configuration/Theme.dart';
 import 'package:gym_track/generic/BlocProvider.dart';
 import 'package:gym_track/generic/Buttons.dart';
-import 'package:gym_track/generic/ModelState.dart';
+import 'package:gym_track/generic/FitSingleChildScrollView.dart';
 import 'package:gym_track/generic/Validators.dart';
-import 'package:gym_track/screens/home/HomePage.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:gym_track/viewmodel/UserViewModel.dart';
+import 'package:rxdart/rxdart.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -21,33 +19,36 @@ class LoginScreen extends StatelessWidget {
         title: Text("Login"),
       ),
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.only(left: 42, right: 42, bottom: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              _logo,
-              TextField(
-                autocorrect: false,
-                autofocus: true,
-                decoration: InputDecoration(hintText: "Email"),
-                maxLines: 1,
-                onChanged: _emailChanged,
-              ),
-              SizedBox(height: 18),
-              TextField(
-                autocorrect: false,
-                autofocus: true,
-                decoration: InputDecoration(hintText: "Password"),
-                maxLines: 1,
-                obscureText: true,
-                onChanged: _passwordChanged,
-              ),
-              SizedBox(height: 42),
-              _loginButton(context, loginScreenBloc),
-              SizedBox(height: 8),
-              _registerButton(context)
-            ],
+        child: BoundedSingleChildScrollView(
+          minHeight: 450,
+          child: Padding(
+            padding: EdgeInsets.only(left: 42, right: 42, bottom: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                _logo,
+                TextField(
+                  autocorrect: false,
+                  autofocus: true,
+                  decoration: InputDecoration(hintText: "Email"),
+                  maxLines: 1,
+                  onChanged: _emailChanged,
+                ),
+                SizedBox(height: 18),
+                TextField(
+                  autocorrect: false,
+                  autofocus: true,
+                  decoration: InputDecoration(hintText: "Password"),
+                  maxLines: 1,
+                  obscureText: true,
+                  onChanged: _passwordChanged,
+                ),
+                SizedBox(height: 42),
+                _loginButton(context, loginScreenBloc),
+                SizedBox(height: 8),
+                _registerButton(context)
+              ],
+            ),
           ),
         ),
       ),
@@ -73,29 +74,33 @@ class LoginScreen extends StatelessWidget {
       fit: FlexFit.loose,
       child: Hero(
         tag: "logo",
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-                child: SvgPicture.asset(AppIcons.AppIconPath,
-                    width: 100, height: 100)),
-            Container(height: 24),
-            Text(
-              "Welcome back",
-              style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  color: ApplicationTheme.current.textColor,
-                  fontSize: 20,
-                  decorationStyle: TextDecorationStyle.double,
-                  decoration: TextDecoration.combine([])),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                  child: SvgPicture.asset(AppIcons.AppIconPath,
+                      width: 100, height: 100)),
+              Container(height: 24),
+              Text(
+                "Welcome back",
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    color: ApplicationTheme.current.textColor,
+                    fontSize: 20,
+                    decorationStyle: TextDecorationStyle.double,
+                    decoration: TextDecoration.combine([])),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   void _passwordChanged(String text) {}
+
   void _emailChanged(String text) {}
 }
 
@@ -105,13 +110,17 @@ class LoginScreenBloc implements BlocBase {
   UserViewModel _userViewModel;
 
   PublishSubject<bool> _loadingSubject = PublishSubject<bool>();
+
   Observable<bool> get isLoading => _loadingSubject.stream;
 
   PublishSubject<String> _errorEmailSubject = PublishSubject<String>();
+
   Observable<String> get errorEmail => _errorEmailSubject.stream;
 
   String _email = "";
+
   get email => _email;
+
   set(value) {
     _email = value;
     if (_currentEmailError != null) _validateForm();
